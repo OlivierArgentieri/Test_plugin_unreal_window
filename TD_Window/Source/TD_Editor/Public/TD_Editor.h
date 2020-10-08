d18739c10bd854b2c7437f425add7c36ae5d8a7f
OlivierArgentieri <olivierargentieri@gmail.com>
2020-10-09 00:21:20 +0200

    appear to work but not loggable

---
+++ TD_Window/Source/TD_Editor/Public/TD_Editor.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "Modules/ModuleInterface.h"
+
+#include "UnrealEd.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(TD_Editor, All, All)
+
+class FTD_EditorModule : public IModuleInterface
+{
+public:
+    virtual void StartupModule() override;
+    virtual void ShutdownModule() override;
+
+};
