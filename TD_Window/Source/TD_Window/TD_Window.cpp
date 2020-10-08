149214bedf01d777a7e803e60dde812884b21d9c
OlivierArgentieri <olivierargentieri@gmail.com>
2020-10-08 22:37:02 +0200

    add project

---
+++ TD_Window/Source/TD_Window/TD_Window.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TD_Window.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TD_Window, "TD_Window" );
